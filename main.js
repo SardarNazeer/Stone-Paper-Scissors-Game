b1efebf2527154bd4c2b150db848add5c9e86806
SardarNazeer <sardarnazeer1108@gmail.com>
2024-05-03 18:54:00 +0500

    completed

--- main.js
+++ main.js
@@ -5,7 +5,18 @@ let comp = 0;
 
 // for selecting the boxes 
 
-const choices = document.querySelectorAll(".choice")
+const choices = document.querySelectorAll(".choice");
+
+// for showing the messsage 
+
+const msg = document.querySelector("#msg");
+
+// for accesing the user and computer score paragraph 
+
+const userScorePara = document.querySelector("#user-score");
+const compScorePara = document.querySelector("#comp-score");
+
+// generating the computer choice randomly 
 
 const genCompChoice = () => {
 
@@ -16,10 +27,63 @@ const genCompChoice = () => {
     return option[randomidx];
 }
 
+// drawgame ke liye function
+
+const drawGame = () => {
+    msg.innerText = "Game was draw play again!";
+    msg.style.backgroundColor = "#081b31";
+};
+
+// winner show karne ke liye 
+
+const showWinner = (userWin, userChoice, compChoice) => {
+    if(userWin){
+        user++;
+        
+        // for updating the user score para 
+
+        userScorePara.innerText = user;
+
+        msg.innerText = `You Win! Your ${userChoice} beats ${compChoice}`;
+        msg.style.backgroundColor = "green";
+    } else {
+        comp++;
+        
+        // for updating the user score para 
+
+        compScorePara.innerText = comp;
+        msg.innerText = `You lost! ${compChoice} beats your ${userChoice}`;
+        msg.style.backgroundColor = "red";
+    }
+}
+
 // computer ke selection ke liye 
 
 const playGame = (userChoice) => {
     const compChoice = genCompChoice();
+
+    if(userChoice === compChoice){
+        // draw game 
+        drawGame();
+    } else {
+        let userWin = true;
+        if(userChoice === "rock"){
+
+            // scissor, paper 
+
+            userWin = compChoice === "paper" ? false : true;
+        } else if(userChoice === "paper"){
+
+            // rock, scissor 
+
+            userWin = compChoice === "scissor" ? false : true;
+        } else {
+            // rock, paper 
+
+            userWin = compChoice === "rock" ? false : true;
+        }
+        showWinner(userWin, userChoice, compChoice);
+    }
 }
 
 // har choice pe addEventListener lagane ke liye 
